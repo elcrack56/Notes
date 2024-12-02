a3c6464c71bf96733f6b20106bfb7b38d73cafe4
elcrack56 <darwinsteven3@gmail.com>
2024-12-01 20:10:10 -0500

    definicion del modelo y actualizacion de la pagina y de la pagina note

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -1,4 +1,4 @@
-namespace Notes;
+namespace Notes.Views;
 
 public partial class AboutPage : ContentPage
 {
@@ -8,6 +8,9 @@ public partial class AboutPage : ContentPage
 	}
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
-        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+        if (BindingContext is Models.About about)
+        {
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
     }
 }
